e36e3ce4f4967b2fa9f56018d312192a5ef6b1ba
Neel Patel <neelpatel6294@gmail.com>
2017-11-23 12:53:45 -0500

    Second Commit

--- app/src/main/java/com/example/patel/livenews/MainActivity.java
+++ app/src/main/java/com/example/patel/livenews/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.patel.livenews;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
@@ -8,11 +9,12 @@ import com.example.patel.livenews.Adapter.LiveNews;
 import com.example.patel.livenews.Adapter.LiveNewsAdapter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private  static final String TECH_REQUEST_URL =
+    private static final String TECH_REQUEST_URL =
             "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=0bd6ee1e25c04e1eaf3ab67459d8e684";
 
 
@@ -23,12 +25,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView liveNews = (ListView)findViewById(R.id.main_listView);
+        ListView liveNews = (ListView) findViewById(R.id.main_listView);
 
 
         // Create a new adapter that takes an empty list of news as input
-        mAdapter = new LiveNewsAdapter(this,new ArrayList<LiveNews>());
+        mAdapter = new LiveNewsAdapter(this, new ArrayList<LiveNews>());
 
         liveNews.setAdapter(mAdapter);
+
+
+        LiveNewsAsyncTask task = new LiveNewsAsyncTask();
+        task.execute(TECH_REQUEST_URL);
+    }
+
+
+    private class LiveNewsAsyncTask extends AsyncTask<String, Void, List<LiveNews>> {
+
+        @Override
+        protected List<LiveNews> doInBackground(String... urls) {
+
+            //if Url is null
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+
+            List<LiveNews> result = QueryUtils.fetchLiveNewsData(urls[0]);
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(List<LiveNews> liveNews) {
+            //Clear adapter of previous news data
+            mAdapter.clear();
+            if(liveNews != null || !liveNews.isEmpty()){
+                mAdapter.addAll(liveNews);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/patel/livenews/QueryUtils.java
+++ app/src/main/java/com/example/patel/livenews/QueryUtils.java
@@ -1,12 +1,166 @@
 package com.example.patel.livenews;
 
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.example.patel.livenews.Adapter.LiveNews;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by PATEL on 11/23/2017.
  */
 
 public class QueryUtils {
 
+    private static final String LOG_TAG = QueryUtils.class.getSimpleName();
+
+
+    //constructor
     public QueryUtils() {
     }
-    
+
+    //requesting live news & returns list of News Objects
+    public static List<LiveNews> fetchLiveNewsData(String requestUrl) {
+
+        //create url object
+        URL url = createUrl(requestUrl);
+
+        //perform http request to url and receive json data
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            System.out.println("ERROR");
+        }
+
+        //extract relevant fields from json reponse and creat a list of news
+        List<LiveNews> liveNewsList = extractFeatureFromJson(jsonResponse);
+        return liveNewsList;
+    }
+
+
+
+    //return new url Object from given string Url
+    private static URL createUrl(String requestUrl) {
+        URL url = null;
+        try {
+            url = new URL(requestUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        //if url is null,then return early;
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        // If the request was successful (response code 200),
+        // then read the input stream and parse the response.
+
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            //If the request was successful(Response Code 200),
+            //then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "ERROR response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    //Covert inputStream into a string which contains the
+    //whole Json response from the server
+    private static String readFromStream(InputStream inputStream) throws IOException {
+
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    //return list of objects that has been built up from
+    //parsing the given Json response
+    private static List<LiveNews> extractFeatureFromJson(String jsonResponse) {
+
+        if (TextUtils.isEmpty(jsonResponse)){
+            return null;
+        }
+
+        //Create new ArrayList that we can start adding News to
+        List<LiveNews> liveNews = new ArrayList<>();
+
+        try{
+
+            //create JSONObject from the Json response string
+            JSONObject baseJsonObject = new JSONObject(jsonResponse);
+
+            //Extract JSONArray associated with the key called "articles"
+            JSONArray newsArray = baseJsonObject.getJSONArray("articles");
+
+            //for each news in newsarray, create object
+            for (int i = 0; i <newsArray.length() ; i++) {
+
+                JSONObject currentLiveNews = newsArray.getJSONObject(i);
+
+                String titleString =  currentLiveNews.getString("title");
+                String authors = currentLiveNews.getString("description");
+
+                LiveNews news = new LiveNews(titleString,authors);
+                liveNews.add(news);
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return liveNews;
+    }
+
+
 }
